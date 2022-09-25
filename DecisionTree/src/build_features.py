4795eb31b6f2c6b4bbdfc781399fb3c69f736360
Chris Stevenson <chris.stevenson@live.com>
2022-09-24 19:42:06 -0600

    Add functionality to fix unknown attribute values.

--- DecisionTree/src/build_features.py
+++ DecisionTree/src/build_features.py
@@ -5,11 +5,11 @@ import numpy as np
 from typing import Tuple
 
 
-def build_features(dataset, test=False) -> Tuple[pd.DataFrame, dict, set]:
+def build_features(dataset, fix_unknown=False, test=False) -> Tuple[pd.DataFrame, dict, set]:
     if dataset == "car":
         return build_car_features(test)
     if dataset == "bank":
-        return build_bank_features(test)
+        return build_bank_features_majority_complete(test) if fix_unknown else build_bank_features(test)
     raise Exception(f"'{dataset}' is not a known dataset.")
 
 
@@ -65,5 +65,35 @@ def build_bank_features(test=False):
     return (df, attributes, set(df["label"].unique()))
 
 
+def build_bank_features_majority_complete(test=False):
+    path_to_columns = os.path.join("data", "bank", "columns.txt")
+    columns = []
+    attributes = dict()
+    with open(path_to_columns, "r") as f:
+        columns = list(map(lambda s: s.strip(), f.readlines()))
+    data_filename = "test.csv" if test else "train.csv"
+    path_to_data = os.path.join("data", "bank", data_filename)
+    df: pd.DataFrame = pd.read_csv(path_to_data, names=columns)
+    for column in columns:
+        mode = df[column].value_counts()
+        new = mode.index[0]
+        if new == "unknown":
+            new = mode.index[1]
+        df[column].replace("unknown", new, inplace=True)
+    for numeric_column_name in ("age", "balance", "day", "duration", "campaign", "pdays", "previous"):
+        median = df[numeric_column_name].median()
+        names = [f"<{median}", f"{median}+"]
+        bins = [-np.inf, median, np.inf]
+        df[numeric_column_name] = pd.cut(df[numeric_column_name], bins, labels=names)
+    for categorical_column_name in ("job", "marital", "education", "contact", "month", "poutcome"):
+        df[categorical_column_name] = df[categorical_column_name].astype("category")
+    for bool_column_names in ("default", "housing", "loan", "label"):
+        df[bool_column_names] = df[bool_column_names].replace(("yes", "no"), (True, False))
+    attribute_keys = set(columns) - {"label"}
+    attributes = dict(zip(attribute_keys, map(lambda c: list(df[c].unique()), attribute_keys)))
+    return (df, attributes, set(df["label"].unique()))
+
+
 if __name__ == "__main__":
-    df, *_ = build_features("shy")
+    df, *_ = build_bank_features_majority_complete()
+    print(df)
--- DecisionTree/src/main.py
+++ DecisionTree/src/main.py
@@ -14,13 +14,13 @@ def report_test_predictions(dataset="car"):
         print(f"Prediction score: {round(dt.test_accuracy(test_df)*100, 4)}%.")
 
 
-def compare_depth_and_split(dataset: str, max_depth=6) -> list[dict]:
+def compare_depth_and_split(dataset: str, fix_unknown=False, max_depth=6) -> list[dict]:
     print(f"Comparing split functions for {dataset} for max depth {max_depth} (this may take awhile)...")
     results_table = []
     for depth in range(1, max_depth + 1):
         print(f"Reached depth {depth}.")
         for f in (split_information_gain, gini, majority_error_split):
-            dt = DecisionTree(*build_features(dataset), split_func=f, max_depth=depth)
+            dt = DecisionTree(*build_features(dataset, fix_unknown=fix_unknown), split_func=f, max_depth=depth)
             for mode in ("test", "training"):
                 test_df, _, _ = build_features(dataset=dataset, test=mode == "test")
                 accuracy = dt.test_accuracy(test_df)
@@ -33,14 +33,14 @@ def compare_depth_and_split(dataset: str, max_depth=6) -> list[dict]:
 def store_results(dataset: str, results_table: list[dict]):
     repr_filename = os.path.join("reports", "python", f"{dataset}_decision_tree_comparison")
     print(f"Outputting Python repr to file {repr_filename}...", end=" ")
-    with open(repr_filename + ".py", "w") as f:
+    with open(repr_filename + ".py", "w+") as f:
         print(repr(results_table), file=f)
     print("Done.")
     results_df = pd.DataFrame(results_table)
     print(results_df)
     tex_filename = os.path.join("reports", "tex", f"{dataset}_decision_tree_comparison")
     print(f"Outputting LaTeX table to file {tex_filename}...", end=" ")
-    with open(tex_filename + ".tex", "w") as f:
+    with open(tex_filename + ".tex", "w+") as f:
         print(results_df.style.to_latex(), file=f)
     print("Done.")
 
@@ -48,4 +48,4 @@ def store_results(dataset: str, results_table: list[dict]):
 if __name__ == "__main__":
     dataset = "bank"
     results_table = compare_depth_and_split(dataset, max_depth=16)
-    store_results(dataset, results_table)
+    store_results("bank_replaced", results_table)
