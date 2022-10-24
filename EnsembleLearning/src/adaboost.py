8285a915829c41e607dc457967e33e056e8ec8c8
Chris Stevenson <chris.stevenson@live.com>
2022-10-24 00:06:30 -0600

    Update adaboost to use _weight df column

--- EnsembleLearning/src/adaboost.py
+++ EnsembleLearning/src/adaboost.py
@@ -1,26 +1,25 @@
 import numpy as np
 
-from . import split_functions
 from .decision_tree import DecisionTree
 
 
 class DecisionStump(DecisionTree):
-    def __init__(self, df, attributes, labels, w):
-        self.w = w
+    def __init__(self, df, attributes, labels):
         super().__init__(df, attributes, labels, max_depth=1)
 
-    def error(self, df, w):
+    def error(self, df):
         total_error = np.finfo(float).eps
         for i in df.index:
             prediction = self.predict(df.iloc[i])
             if prediction != df["label"].iloc[i]:
-                total_error += w[i]
+                total_error += df["_weight"][i]
         return total_error
 
 
 class AdaBoost(object):
     def __init__(self, df, attributes, t):
         self.df = df
+        df["_weight"] = np.ones(len(df.index)) / len(df.index)
         self.attibutes = attributes
         self.t = t
         self.votes, self.errors = adaboost(df, attributes, t)
@@ -41,21 +40,22 @@ class AdaBoost(object):
         return correct_ids / len(dataset)
 
 
+def update_weight(row, stump, a):
+    if stump.predict(row) == row["label"]:
+        return row["_weight"] * np.exp(-a)
+    return row["_weight"] * np.exp(a)
+
+
 def adaboost(df, attributes, t):
     votes = dict()
-    w = np.ones(len(df)) / len(df)
     errors = []
     for i in range(t):
-        stump = DecisionStump(df, attributes, df["label"].unique(), w)
-        error = stump.error(df, w)
+        stump = DecisionStump(df, attributes, df["label"].unique())
+        error = stump.error(df)
         errors.append((i, error))
         a = 0.5 * np.log((1 - error) / error)
         votes[stump] = a
-        for j in df.index:
-            predicted_correctly = stump.predict(df.iloc[j]) == df["label"].iloc[j]
-            if predicted_correctly:
-                w[j] *= np.exp(-a)
-            else:
-                w[j] *= np.exp(a)
-        w = w / w.sum()
+        _update_weight = lambda r: update_weight(r, stump, a)
+        df.apply(_update_weight, axis=1)
+        df["_weight"] /= df["_weight"].sum()
     return votes, errors
