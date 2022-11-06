240f091ca4719fab848a18fa615c73852bf52810
Chris Stevenson <chris.stevenson@live.com>
2022-11-06 13:46:49 -0700

    Add parse bank note function

--- Perceptron/src/main.py
+++ Perceptron/src/main.py
@@ -3,8 +3,22 @@ import argparse
 import numpy as np
 
 
+def sgn(x):
+    return np.where(x > 0, +1, -1)
+
+
+def parse_bank_note_csv():
+    train_csv = np.loadtxt("data/bank-note/train.csv", delimiter=",")
+    _X, y = train_csv[:, :-1], sgn(train_csv[:, -1])
+    m, _ = _X.shape
+    X = np.concatenate((np.ones((m, 1)), _X), axis=1)
+    return X, y
+
+
 def q2a():
-    pass
+    X, y = parse_bank_note_csv()
+    w = np.zeros(X.shape[1])
+    print(w)
 
 
 if __name__ == "__main__":
